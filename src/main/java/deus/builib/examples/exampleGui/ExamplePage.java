5d8ca7dcae849b6784d27e713719184aa824abdf
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-12-28 17:14:47 +0100

    * Deleted hardcoded path

--- src/main/java/deus/builib/examples/exampleGui/ExamplePage.java
+++ src/main/java/deus/builib/examples/exampleGui/ExamplePage.java
@@ -9,12 +9,13 @@ public class ExamplePage extends Page {
 	public ExamplePage(Router router, String... text) {
 		super(GuiLib.class, router);
 
-		//styleSheetPath = "C:\\Users\\masit\\IdeaProjects\\BTA-GuiLibrary\\run\\GuiLibrary\\TestFolder\\styles.yaml";
-		xmlPath = "C:\\Users\\masit\\IdeaProjects\\BTA-GuiLibrary\\run\\GuiLibrary\\TestFolder\\test.xml";
+		xmlPath = "";
 
-		// ! DON'T DELETE IT
-		reloadXml();
-		reloadStyles();
+		setup(()->{
+
+			// Your code here
+
+		});
 
 	}
 }
